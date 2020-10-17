85071de0ab3480d5478171d3150a35a8805e2b1a
pmiakish <mvecktor@gmail.com>
2020-10-17 15:39:18 +0300

    output message has been changed

--- module_1/src/com/epam/Mod1Task5.java
+++ module_1/src/com/epam/Mod1Task5.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Mod1Task5 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.print ("Enter the natural number (non-negative integer > 0): ");
+        System.out.print ("Enter the natural number (non-negative integer greater than 0): ");
         int T = console.nextInt();
             if (T>=1) {
                 System.out.printf ("Expression value = %02dh %02dm %02ds", T/3600, T%3600/60, T%60);
