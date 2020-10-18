89ca6143d14e16814bd9469bc06bdc44d4b768d8
pmiakish <mvecktor@gmail.com>
2020-10-18 22:31:59 +0300

    sum of numbers from 1 to A

---
+++ module_1/src/com/epam/cycles/CyclesTaskA.java
@@ -0,0 +1,24 @@
+package com.epam.cycles;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class CyclesTaskA {
+
+    public static void main(String[] args) {
+
+    Scanner console = new Scanner(System.in).useLocale(Locale.US);
+    System.out.print ("Enter the natural number A (non-negative integer greater than or equal to 1): ");
+    int a = console.nextInt();
+
+        if (a >= 1) {
+            int sum = 0;
+                for (int i = 1; i <= a; i++) {
+                    sum += i;
+                }
+            System.out.println("Total amount is " + sum);
+        } else {
+            System.out.println("The number must be greater than or equal to 1!");
+        }
+
+    }
+}
