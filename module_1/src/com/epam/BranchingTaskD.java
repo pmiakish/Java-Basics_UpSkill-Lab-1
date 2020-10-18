2361b35671709802c01e6ef37d034589f1b5911b
pmiakish <mvecktor@gmail.com>
2020-10-18 13:15:03 +0300

    A brick & an aperture

---
+++ module_1/src/com/epam/BranchingTaskD.java
@@ -0,0 +1,26 @@
+package com.epam;
+import java.util.Scanner;
+import java.util.Locale;
+import java.util.Arrays;
+
+public class BranchingTaskD {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println ("Enter the rectangle aperture's dimensions (A, B): ");
+        System.out.print ("A value: ");
+        double a = console.nextDouble();
+        System.out.print ("B value: ");
+        double b = console.nextDouble();
+        System.out.println ("Enter the brick dimensions (x, y, z): ");
+        double[] brickDimensions = new double[3];
+        System.out.print ("x value: ");
+        brickDimensions[0] = console.nextDouble();
+        System.out.print ("y value: ");
+        brickDimensions[1] = console.nextDouble();
+        System.out.print ("z value: ");
+        brickDimensions[2] = console.nextDouble();
+        Arrays.sort(brickDimensions);
+        if (Math.min(a,b) >= brickDimensions[0] && Math.max(a,b) >= brickDimensions[1]) System.out.println ("The aperture matches");
+        else System.out.println ("The aperture doesn't match");
+    }
+}
