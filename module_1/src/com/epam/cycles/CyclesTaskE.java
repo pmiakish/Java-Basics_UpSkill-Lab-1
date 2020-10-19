b0c03070b13b181482d83228b49f206e0e011586
pmiakish <mvecktor@gmail.com>
2020-10-20 00:24:57 +0300

    symbols and their numerical representations

---
+++ module_1/src/com/epam/cycles/CyclesTaskE.java
@@ -0,0 +1,14 @@
+package com.epam.cycles;
+
+public class CyclesTaskE {
+
+    private static final int  ASCII_LENGTH = 255;
+
+    public static void main(String[] args) {
+        System.out.println("Mapping table of symbols and their numerical representations:");
+        for (int i = 0; i <= ASCII_LENGTH; i++) {
+           System.out.printf("%03d  - ", i);
+           System.out.println((char) i);
+        }
+    }
+}
