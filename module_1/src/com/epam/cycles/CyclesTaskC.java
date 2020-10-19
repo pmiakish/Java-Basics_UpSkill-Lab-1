e537f8574adb1260a5139d56cb2f8106c8d51ee1
pmiakish <mvecktor@gmail.com>
2020-10-19 22:28:49 +0300

    spaces were added

--- module_1/src/com/epam/cycles/CyclesTaskC.java
+++ module_1/src/com/epam/cycles/CyclesTaskC.java
@@ -7,7 +7,7 @@ public class CyclesTaskC {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 1; i <= AMOUNT_OF_NUMBERS; i++) {
-            sum += i*i;
+            sum += i * i;
         }
         System.out.println("The sum of the first " + AMOUNT_OF_NUMBERS + " square numbers is " + sum);
     }
