423dbe06ea39ba01d416f74471efcc32de331f20
pmiakish <mvecktor@gmail.com>
2020-10-19 21:11:55 +0300

    The sum of the first 100 square numbers

---
+++ module_1/src/com/epam/cycles/CyclesTaskC.java
@@ -0,0 +1,14 @@
+package com.epam.cycles;
+
+public class CyclesTaskC {
+
+    private static final int AMOUNT_OF_NUMBERS = 100;
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= AMOUNT_OF_NUMBERS; i++) {
+            sum += i*i;
+        }
+        System.out.println("The sum of the first " + AMOUNT_OF_NUMBERS + " square numbers is " + sum);
+    }
+}
