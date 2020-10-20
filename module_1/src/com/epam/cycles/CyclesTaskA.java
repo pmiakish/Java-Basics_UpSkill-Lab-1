76175d2f18682933be2b5cf49fd069c0997f0a03
pmiakish <mvecktor@gmail.com>
2020-10-20 21:50:30 +0300

    excess tabulation and spaces were deleted

--- module_1/src/com/epam/cycles/CyclesTaskA.java
+++ module_1/src/com/epam/cycles/CyclesTaskA.java
@@ -6,18 +6,18 @@ public class CyclesTaskA {
     public static void main(String[] args) {
 
     Scanner console = new Scanner(System.in);
-    System.out.print ("Enter the natural number A (non-negative integer greater than or equal to 1): ");
+    System.out.print("Enter the natural number A (non-negative integer greater than or equal to 1): ");
     int a = console.nextInt();
 
-        if (a >= 1) {
-            int sum = 0;
-                for (int i = 1; i <= a; i++) {
-                    sum += i;
-                }
-            System.out.println("Total amount is " + sum);
-        } else {
-            System.out.println("The number must be greater than or equal to 1!");
+    if (a >= 1) {
+        int sum = 0;
+        for (int i = 1; i <= a; i++) {
+            sum += i;
         }
+        System.out.println("Total amount is " + sum);
+    } else {
+        System.out.println("The number must be greater than or equal to 1!");
+    }
 
     }
 }
