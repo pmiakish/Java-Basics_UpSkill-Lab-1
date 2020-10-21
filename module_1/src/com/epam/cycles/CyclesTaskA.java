c4a7f7dbba46c49907ea710413535401e054ed3d
pmiakish <mvecktor@gmail.com>
2020-10-21 19:00:07 +0300

    tabulation has been fixed, int type to long type has been changed

--- module_1/src/com/epam/cycles/CyclesTaskA.java
+++ module_1/src/com/epam/cycles/CyclesTaskA.java
@@ -5,19 +5,19 @@ public class CyclesTaskA {
 
     public static void main(String[] args) {
 
-    Scanner console = new Scanner(System.in);
-    System.out.print("Enter the natural number A (non-negative integer greater than or equal to 1): ");
-    int a = console.nextInt();
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter the natural number A (non-negative integer greater than or equal to 1): ");
+        long a = console.nextLong();
 
-    if (a >= 1) {
-        int sum = 0;
-        for (int i = 1; i <= a; i++) {
-            sum += i;
+        if (a >= 1) {
+            long sum = 0;
+            for (long i = 1; i <= a; i++) {
+                sum += i;
+            }
+            System.out.println("Total amount is " + sum);
+        } else {
+            System.out.println("The number must be greater than or equal to 1!");
         }
-        System.out.println("Total amount is " + sum);
-    } else {
-        System.out.println("The number must be greater than or equal to 1!");
-    }
 
     }
 }
