d37af29db3deb1905f3c346645961957ef346370
pmiakish <71881598+pmiakish@users.noreply.github.com>
2020-10-21 21:06:32 +0300

    Module1 cycles task1 (The sum of natural numbers) (#12)
    * sum of numbers from 1 to A
    
    * import of class java.util..Locale has been deleted
    
    * excess tabulation and spaces were deleted
    
    * tabulation has been fixed, int type to long type has been changed

---
+++ module_1/src/com/epam/cycles/CyclesTaskA.java
@@ -0,0 +1,23 @@
+package com.epam.cycles;
+import java.util.Scanner;
+
+public class CyclesTaskA {
+
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter the natural number A (non-negative integer greater than or equal to 1): ");
+        long a = console.nextLong();
+
+        if (a >= 1) {
+            long sum = 0;
+            for (long i = 1; i <= a; i++) {
+                sum += i;
+            }
+            System.out.println("Total amount is " + sum);
+        } else {
+            System.out.println("The number must be greater than or equal to 1!");
+        }
+
+    }
+}
