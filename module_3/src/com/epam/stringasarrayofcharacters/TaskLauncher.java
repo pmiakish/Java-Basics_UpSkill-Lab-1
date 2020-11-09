fe2446dbf93e38f06de8638fd767c6f8113535a8
pmiakish <mvecktor@gmail.com>
2020-11-10 02:07:11 +0300

    modified TaskLauncher.java (added the final array with names of variables)

--- module_3/src/com/epam/stringasarrayofcharacters/TaskLauncher.java
+++ module_3/src/com/epam/stringasarrayofcharacters/TaskLauncher.java
@@ -3,11 +3,15 @@ package com.epam.stringasarrayofcharacters;
 import java.util.Arrays;
 
 public class TaskLauncher {
+
+    // task 01
+    private static final String[] NAMES_OF_VARIABLES =
+            {"camelCase", "bestNameOfVariable", "stringVariable", "itemOfArray", "lastName"};
+
     public static void main(String[] args) {
         // task 01
-        String[] namesOfVariables = {"camelCase", "bestNameOfVariable", "stringVariable", "itemOfArray", "lastName"};
-        StringBuilder[] renamedVariables = StringModifier.changeCamelCaseToSnakeCase(namesOfVariables);
-        System.out.println("\n--- TASK 01 ---\nThe camelCase names: " + Arrays.toString(namesOfVariables) + "\n" +
+        StringBuilder[] renamedVariables = StringModifier.changeCamelCaseToSnakeCase(NAMES_OF_VARIABLES);
+        System.out.println("\n--- TASK 01 ---\nThe camelCase names: " + Arrays.toString(NAMES_OF_VARIABLES) + "\n" +
                 "The snake_case names: " + Arrays.toString(renamedVariables));
     }
 }
